3996ac732c82eaa35b79a43206e70ddca297b8bd
MayurChawla <mr.mayurchawla@gmail.com>
2021-07-08 10:13:00 +0530

    added available button

--- checkbox.js
+++ checkbox.js
@@ -38,3 +38,14 @@ function select1()
         top2.checked = true;
     }
 }
+
+function showAvailable()
+{
+    let all = [];
+    for(let i=0; i<nontop2.length; i++)
+    {
+        if(!nontop2[i].checked)
+            all[i] = (i+1);
+    }
+    document.getElementById("available").innerHTML = "Available : " + all;
+}
